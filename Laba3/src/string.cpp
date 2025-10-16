f62c7f1e9f98e40185c809a3dbfa9d7e9f979610
PeTkun_Industries <romano22854566@gmail.com>
2025-10-17 01:37:18 +0300

    COMMIT

--- Laba3/src/string.cpp
+++ Laba3/src/string.cpp
@@ -1,11 +1,19 @@
 #include "string.hpp"
 #include <cstring>
 
-String::String(): data(nullptr),length(0) {}
 
+static std::size_t myStrLen(const char* s) {
+    if (!s) return 0;
+    std::size_t len = 0;
+    while (s[len] != '\0') {
+        ++len;
+    }
+    return len;
+}
+String::String(): data(nullptr),length(0) {}
 String::String(const char* s): data(nullptr),length(0) {
     if (s) {
-        length = std::strlen(s);
+        length = myStrLen(s); 
         data = new char[length + 1];
         std::memcpy(data,s,length);
         data[length] = '\0';
